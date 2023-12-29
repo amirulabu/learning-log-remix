dbc6276c53d80c8d1ca043c660b66d43f8099713
Amirul Abu <amirulabu@gmail.com>
2023-12-29 11:05:31 +0800

    Fix import

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -7,7 +7,7 @@
 import { installGlobals } from "@remix-run/node";
 import { parse } from "cookie";
 
-import { createUser } from "~/models/user.server";
+import { getOrCreateUser } from "~/models/user.server";
 import { createUserSession } from "~/session.server";
 
 installGlobals();
@@ -20,7 +20,7 @@ async function createAndLogin(email: string) {
     throw new Error("All test emails must end in @example.com");
   }
 
-  const user = await createUser(email, "myreallystrongpassword");
+  const user = await getOrCreateUser(email, "google");
 
   const response = await createUserSession({
     request: new Request("test://test"),
